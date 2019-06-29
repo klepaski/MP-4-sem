7b41be02541882fdc06d633d0a217b9d84bb3ba3
Klepaski <43060010+klepaski@users.noreply.github.com>
2019-06-29 14:43:32 +0300

    Add files via upload

---
+++ Lab2/4/4/4.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip> 
+#include "Combi.h"
+
+#define N (sizeof(AA)/2)
+#define M 3
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	 char  AA[][2]= {"A", "B", "C", "D"}; 
+
+	 cout << "****** Generator of placement *******" << endl;
+	 cout << endl << "Primary array: ";
+	 cout << "{ ";
+ 
+	 for (int i = 0; i < N; i++) 
+
+		  cout << AA[i] << ((i< N-1)?", ":" ");  
+		  cout << "}" << endl;
+		  cout << endl << "Generation of placements iz  " << N << " po " << M << ":";
+
+	 combi::accomodation s(N,M);
+
+	 int  n = s.getfirst();      
+ 
+	 while (n >= 0)
+	 {
+		cout << endl << setw(2) << s.na << ": { ";
+
+		for (int i = 0; i < 3; i++)  
+
+			 cout << AA[s.ntx(i)] << ((i< n-1)?", ":" "); 
+			 cout << "}";
+
+			 n = s.getnext();    
+	 };
+
+	 cout << endl << "Vsego: " << s.count() << endl;
+
+	 system("pause");
+	 return 0;
+} 
